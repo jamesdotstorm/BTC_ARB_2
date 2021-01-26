49662e2268450b9d3d19f1f54cbbca4acf9cb097
jamesdotstorm <james@quicket.co.za>
2021-01-26 08:52:14 +0200

    Working now

--- docs/Jamiesapp.js
+++ docs/Jamiesapp.js
@@ -5,7 +5,7 @@ function getJokes(e) {
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'https://api.chucknorris.io/jokes/random', true);
+    xhr.open('GET', 'https://www.bitstamp.net/api/ticker/', true);
 
     xhr.onload = function() {
         if (this.status === 200) {
